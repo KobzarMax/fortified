af02eadf7d6cd40fde30e2e2bc5c187f5280667d
Max Kobzar <nimbus2219@gmail.com>
2024-06-15 23:25:01 +0200

    save

--- src/routes/router-list.jsx
+++ src/routes/router-list.jsx
@@ -1,19 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import {
   ROUTE_COMPONENTS,
-  ROUTE_CONTACT,
+  // ROUTE_CONTACT,
   ROUTE_MAIN,
-  ROUTE_PRIVACY_POLICY,
-  ROUTE_TERMS_OF_USE,
+  // ROUTE_PRIVACY_POLICY,
+  // ROUTE_TERMS_OF_USE,
 } from "./routes";
 import { Layout } from "../components/Layout";
 import ErrorPage from "../pages/ErrorPage";
 import { Home } from "../pages/Home";
 import { Suspense } from "react";
 import { Loader } from "../components/Loader";
-import { Contact } from "../pages/Contact";
-import { TermsOfUse } from "../pages/TermsOfUse";
-import { PrivacyPolicy } from "../pages/PrivacyPolicy";
+// import { Contact } from "../pages/Contact";
+// import { TermsOfUse } from "../pages/TermsOfUse";
+// import { PrivacyPolicy } from "../pages/PrivacyPolicy";
 import { Components } from "../pages/Components";
 
 export const router = createBrowserRouter([
@@ -33,30 +33,30 @@ export const router = createBrowserRouter([
           </Suspense>
         ),
       },
-      {
-        path: ROUTE_CONTACT,
-        element: (
-          <Suspense fallback={<Loader />}>
-            <Contact />
-          </Suspense>
-        ),
-      },
-      {
-        path: ROUTE_TERMS_OF_USE,
-        element: (
-          <Suspense fallback={<Loader />}>
-            <TermsOfUse />
-          </Suspense>
-        ),
-      },
-      {
-        path: ROUTE_PRIVACY_POLICY,
-        element: (
-          <Suspense fallback={<Loader />}>
-            <PrivacyPolicy />
-          </Suspense>
-        ),
-      },
+      // {
+      //   path: ROUTE_CONTACT,
+      //   element: (
+      //     <Suspense fallback={<Loader />}>
+      //       <Contact />
+      //     </Suspense>
+      //   ),
+      // },
+      // {
+      //   path: ROUTE_TERMS_OF_USE,
+      //   element: (
+      //     <Suspense fallback={<Loader />}>
+      //       <TermsOfUse />
+      //     </Suspense>
+      //   ),
+      // },
+      // {
+      //   path: ROUTE_PRIVACY_POLICY,
+      //   element: (
+      //     <Suspense fallback={<Loader />}>
+      //       <PrivacyPolicy />
+      //     </Suspense>
+      //   ),
+      // },
       {
         path: ROUTE_COMPONENTS,
         element: (
